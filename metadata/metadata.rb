5b5bd2d5088300fe761598113b8000120237c0f2
Brendan Hay <brendan.g.hay@gmail.com>
2014-02-06 11:17:31 +0100

    Updating with bind alias'

--- metadata/metadata.rb
+++ metadata/metadata.rb
@@ -5,7 +5,7 @@ require 'rack'
 require 'rubydns'
 require 'thin'
 
-PORT     = 80
+BIND     = '169.254.169.254'
 IN       = Resolv::DNS::Resource::IN
 UPSTREAM = RubyDNS::Resolver.new([
   [:tcp, '8.8.8.8', 53],
@@ -13,43 +13,50 @@ UPSTREAM = RubyDNS::Resolver.new([
 ])
 
 def self.run
-  dns = RubyDNS::RuleBasedServer.new do
-    match("instance-data", IN::A) do |tx|
-      tx.respond!('127.0.0.1')
+  begin
+    puts "Adding #{BIND} to lo0"
+    system("ifconfig lo0 alias #{BIND}")
+
+
+    dns = RubyDNS::RuleBasedServer.new do
+      match("instance-data", IN::A) do |tx|
+        tx.respond!('127.0.0.1')
+      end
+
+      otherwise do |tx|
+        tx.passthrough!(UPSTREAM)
+      end
     end
 
-    otherwise do |tx|
-      tx.passthrough!(UPSTREAM)
+    dir = Rack::Builder.new do
+      run Rack::Directory.new(File.expand_path(File.dirname(__FILE__)))
     end
-  end
 
-  files = Rack::Builder.new do
-    run Rack::Directory.new(File.expand_path(File.dirname(__FILE__)))
-  end
+    app   = Rack::Chunked.new(Rack::ContentLength.new(dir))
+    files = Thin::Server.new(BIND, 80, app)
 
-  stop = Proc.new do
-    dns.fire(:stop)
-    EventMachine.stop
-  end
+    Signal.trap('INT', EventMachine.stop)
+    Signal.trap('TERM', EventMachine.stop)
 
-  Signal.trap('INT', &stop)
-  Signal.trap('TERM', &stop)
+    EventMachine.run do
+      puts 'Redirecting http://instance-data to localhost'
+      dns.run(:listen => [
+        [:tcp, '0.0.0.0', 53],
+        [:udp, '0.0.0.0', 53]
+      ])
 
-  EventMachine.run do
-    puts 'Redirecting http://instance-data to localhost'
-    dns.run(:listen => [
-      [:tcp, '0.0.0.0', 53],
-      [:udp, '0.0.0.0', 53]
-    ])
-
-    puts 'Serving metadata on http://169.254.169.254/latest'
-    Rack::Handler::Thin.run(files, :Port => PORT)
+      puts "Serving metadata on http://#{BIND}/latest"
+      files.start
+    end
+  ensure
+    puts "Removing #{BIND} from lo0"
+    system("ifconfig lo0 -alias #{BIND}")
   end
 end
 
 if ENV['USER'] == 'root'
   run
 else
-  puts 'Error: Please re-run using sudo due to DNS resolver and port 80 usage.'
+  puts 'Error: Please re-run using sudo due to resolv.conf and port 80 usage.'
   exit(1)
 end

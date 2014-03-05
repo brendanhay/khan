bf4d4bc54beb68b471f5bd9b523af5b38028fdf2
Brendan Hay <brendan.g.hay@gmail.com>
2014-03-05 14:38:45 +0100

    Encoding document output

--- src/Khan/CLI/Metadata.hs
+++ src/Khan/CLI/Metadata.hs
@@ -51,7 +51,7 @@ commands = command "metadata" describe describeParser
 describe :: Common -> Describe -> AWS ()
 describe _ Describe{..} = do
     bs  <- liftEitherT $ Meta.dynamic Document
-    debug "Received dynamic document:\n{}" [bs]
+    debug "Received dynamic document:\n{}" [Text.decodeUtf8 bs]
 
     doc <- filtered <$> decode bs
     iid <- instanceId doc

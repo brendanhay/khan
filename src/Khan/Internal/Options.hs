7c0f00d88183d1e89fae33a27e10f5126d2a3bb3
Brendan Hay <brendan.g.hay@gmail.com>
2014-04-04 10:46:26 +0200

    Adding --no-vpn flag to switch between private IPv4 and public DNS

--- src/Khan/Internal/Options.hs
+++ src/Khan/Internal/Options.hs
@@ -93,6 +93,7 @@ data Common = Common
     , cLKeys  :: !LKeysDir
     , cCache  :: !CacheDir
     , cConfig :: !ConfigDir
+    , cVPN    :: !Bool
     } deriving (Show)
 
 newtype RKeysBucket = RKeysBucket { rKeysBucket :: Text }
@@ -131,6 +132,8 @@ commonParser env = Common
         <> epath "KHAN_CONFIG" env
         <> short 'C'
          ) "Path to configuration files.")
+    <*> switchOption "no-vpn" True
+        "Use public DNS instead of private IPv4 (default) for EC2 addresses."
 
 instance Options Common where
     validate Common{..} = do

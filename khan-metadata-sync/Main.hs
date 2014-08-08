f7b5caff78e2bff3177b0b7105e60ddf218bff63
Brendan Hay <brendan.g.hay@gmail.com>
2014-08-08 16:59:42 +0200

    Ensuring running on an EC2 instance

--- khan-metadata-sync/Main.hs
+++ khan-metadata-sync/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
@@ -13,123 +14,48 @@
 
 module Main (main) where
 
-import           Data.List                (partition)
-import qualified Data.Text                as Text
-import qualified Data.Text.IO             as Text
-import qualified Khan.Model.Host          as Host
-import qualified Khan.Model.SSH           as SSH
+import qualified Data.ByteString.Lazy      as LBS
+import qualified Data.Text                 as Text
+import qualified Data.Text.Lazy            as LText
+import qualified Data.Text.Lazy.Encoding   as LText
+import qualified Filesystem                as FS
+import qualified Filesystem.Path.CurrentOS as Path
+import           Khan.Internal
 import           Khan.Prelude
-import           Khan.Sync.Enumerate
-import           Network.AWS.EC2.Metadata
-
--- data Action
---     = List Text [Action]
---     | Get  Text
---       deriving (Eq, Show)
+import           Network.HTTP.Conduit      (simpleHttp)
+import           Options.Applicative
+import           System.IO
 
 main :: IO ()
 main = do
-    -- mapM_ Text.putStrLn
-    --     . concatMap (pprint 0)
-    --     . merge
-    --     . actions
-    --     $ (enumerate :: [Meta])
+    !d <- execParser (info (directory <**> helper) idm)
 
+    enableLogging
+
+    log_ "Checking if running on an EC2 instance ..."
+    !_ <- simpleHttp "http://instance-data/"
+
+    say "Ensuring {} exists ..." [d]
+    FS.createTree d
+
+    say "Changing working directory to {} ..." [d]
+    FS.setWorkingDirectory d
+
+    log_ "Starting metadata sync ..."
+    retrieve d "latest/"
   where
-    get url = do
-        
+    directory = pathOption "dir"
+        ( value "./instance-data"
+       <> short 'd'
+        ) "Path to output the synced metadata."
 
-curl http://169.254.169.254/latest/meta-data/
+    retrieve d p = do
+        say "Retrieving {} ..." [p]
+        !lbs <- simpleHttp ("http://169.254.169.254/" ++ Text.unpack p)
 
-ami-id
-ami-launch-index
-ami-manifest-path
-block-device-mapping/
-hostname
-instance-action
-instance-id
-instance-type
-kernel-id
-local-hostname
-local-ipv4
-mac
-network/
-placement/
-public-hostname
-public-ipv4
-public-keys/
-reservation-id
-security-groups
-services/
+        say "Writing {} ..." [p]
+        FS.withFile (d </> Path.fromText p) WriteMode (`LBS.hPut` lbs)
 
-    -- actions :: ToPath a => [a] -> [[Action]]
-    -- actions = map (build . Text.split (== '/') . toPath)
-
-    -- build []     = []
-    -- build [x]    = [Get  x]
-    -- build (x:xs) = [List x (build xs)]
-
-    -- merge :: [Action] -> [Action]
-    -- merge = foldr' go []
-    --   where
-    --     go :: Action -> [Action] -> [Action]
-    --     go (List w ws) xs =
-    --         case partition (match w) xs of
-    --             (ys, []) -> [List w (merge (ws ++ ys))]
-    --             (ys, zs) -> List w (merge (ws ++ ys)) : zs
-    --     go x xs = x : xs
-
-    --     match :: Text -> Action -> Bool
-    --     match x (List y _) = x == y
-    --     match _ _          = False
-
-    -- pprint :: Int -> Action -> [Text]
-    -- pprint n x = case x of
-    --     List k xs -> f k : concatMap (pprint (n + 2)) xs
-    --     Get k     -> [f k]
-    --   where
-    --     f = mappend (Text.replicate n " ")
-
---    foldr (a -> b -> b) -> b -> t a -> b
-
---    mapM_ (print . toPath) (enumerate :: [Meta])
-
--- need to save userdata
--- need to list securitycredentials, save, get first, save
-
-
--- ec2 :: (Functor m, MonadIO m) => m Bool
--- ec2 = fmap isRight
---     . runEitherT
---     . syncIO
---     $ simpleHttp "http://instance-data/latest"
-
--- user :: MonadIO m => m (Maybe LBS.ByteString)
--- user = liftIO $ EX.catch (Just <$> simpleHttp url) ex
---   where
---     url = "http://169.254.169.254/latest/user-data"
-
---     ex (StatusCodeException s _ _)
---         | status404 == s = return Nothing
---     ex e                 = EX.throw e
-
--- meta :: (Functor m, MonadIO m) => Meta -> EitherT String m ByteString
--- meta = get "http://169.254.169.254/latest/meta-data/"
-
--- dynamic :: (Functor m, MonadIO m) => Dynamic -> EitherT String m ByteString
--- dynamic = get "http://169.254.169.254/latest/dynamic/"
-
--- get :: (Functor m, MonadIO m, ToPath a)
---     => Text
---     -> a
---     -> EitherT String m ByteString
--- get base p = do
---     rs <- fmapLT show
---         . syncIO
---         . simpleHttp
---         . Text.unpack
---         $ base <> toPath p
-
---     case strip '\n' $ LBS.toStrict rs of
---         "" -> throwError "Failed to receive any data"
---         bs -> return bs
+        when ("/" `Text.isSuffixOf` p) $
+            mapM_ (retrieve d . mappend p)
+                  (Text.lines . LText.toStrict $ LText.decodeUtf8 lbs)

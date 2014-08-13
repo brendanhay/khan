b9b0ceeacba9767f67b96a8f756edeec2964f291
Brendan Hay <brendan.g.hay@gmail.com>
2014-08-13 09:52:03 +0200

    Using bash builtin type, instead of the less common/portable which to check for existence

--- khan/Khan/Internal/IO.hs
+++ khan/Khan/Internal/IO.hs
@@ -60,7 +60,7 @@ sync = fmapLT show . syncIO
 
 which :: String -> EitherT String IO ()
 which cmd = do
-    (c, _, _) <- sync $ readProcessWithExitCode "which" [cmd] ""
+    (c, _, _) <- sync $ readProcessWithExitCode "type" ["-fP", cmd] ""
     case c of
         ExitFailure _ -> throwError ("Failed to find command: " ++ cmd)
         ExitSuccess   -> return ()
